77864b0eb45334f1206870b6918e563fc7dd50e7
Dai Longao <dai@jiong3.cn>
2013-03-27 03:35:42 +0800

    User check && Login files

---
+++ include/checkfunctions.php
@@ -0,0 +1,19 @@
+<?php
+# Discuss System Checking Files
+# 2013.3.16
+
+if(!defined('SAFE_INCLUDE'))
+die('Hacking attempt!');
+
+function getUserLoginStatus(){
+
+}
+
+function getUserClass(){
+
+}
+
+function checkUserLogin($user, $pass){
+
+}
+?>
---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+# Discuss System Login Parts
+# 2013.3.16 Dai
+
+require "include/incfunctions.php"
+dbconn();
+
+if(getUserLoginStatus()){
+
+}
+
+?>
